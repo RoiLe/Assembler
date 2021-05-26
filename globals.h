ee2e6fce2d46df2046b2c85b67c042cb53be29cb
Roi Levi <74343893+RoiLe@users.noreply.github.com>
2021-05-26 19:34:05 +0300

    Update globals.h

--- globals.h
+++ globals.h
@@ -1,4 +1,7 @@
 /*- קובץ header הצהרות על פונקציות שמשתמשים בהם במהלך כל התוכנית (registers,opcode,instructions, IC&DC, symbol-table ודומיהם)*/
+#define IC 100
+#define DC 0
+
 typedef enum success{
 	failed = 0, succesed = 1
 }suc;
@@ -6,6 +9,13 @@ typedef enum success{
 typedef enum booleans {
 	FALSE = 0, TRUE = 1
 } bool;
+
+typedef enum errors{
+	NAME_ERROR = 1;
+
+
+
+}err;
 
 typedef enum registers {
 	$0 = 0,
@@ -42,3 +52,37 @@ typedef enum registers {
 	$31,
 
 } reg;
+
+
+typedef struct instructionR{
+	unsigned int NONE: 5;
+	unsigned int funct: 5;
+	unsigned int rd: 5;
+	unsigned int rt: 5;	
+	unsigned int rs: 5;
+	unsigned int opcode: 6;
+}R;
+
+
+typedef struct instructionI{
+	unsigned int immed: 15;
+	unsigned int rt: 5;	
+	unsigned int rs: 5;
+	unsigned int opcode: 6;
+}I;
+
+typedef struct instructionJ{
+	unsigned int address: 24;	
+	unsigned int reg: 1;
+	unsigned int opcode: 6;
+}J;
+
+typedef struct symboleTable{
+	char * symbol;
+	int value;
+	char *attribute;
+}symTab;
+
+
+
+
