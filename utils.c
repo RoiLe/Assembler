b2e03989b9588a72fc97d86eb98df5af1f2a8098
Roi Levi <74343893+RoiLe@users.noreply.github.com>
2021-05-26 19:35:10 +0300

    Create utils.c

---
+++ utils.c
@@ -0,0 +1,22 @@
+#include "global.h" 
+
+/*this func checks the name of the input file.*/
+int checkNameFile(*char);
+
+/*this func print the correct error*/
+printErrors(int);
+[
+case 0 : the file worked correct.
+case 1 : "file name error"
+]
+
+
+
+
+
+1. buildLRogicsInstructions( ls, rt, rd, funct) --> R
+2. buildRCopyInstructions(rs, rd, funct) --> R
+3. buildILogicsInstructions(funct) --> I
+4. buildIBranchesInstructions(funct) --> I
+5. buildIStoreLoadInstructions(funct) --> I
+6. j!
