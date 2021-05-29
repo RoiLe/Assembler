037a2e01d39c1e6cbe3ce7cb88d5241ea8906ee7
Roi Levi <74343893+RoiLe@users.noreply.github.com>
2021-05-29 22:57:39 +0300

    Update exeptions.c

--- exeptions.c
+++ exeptions.c
@@ -1,1 +1,52 @@
-/*-מחזיק פונקציות המתרגמות קוד לשפה בינארית (מספרים, אוגרים וכד')*/
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "globals.h"
+#include "exeptions.h"
+
+
+
+
+void printErrors(int errorType)
+{
+	switch(errorType)
+    	{
+	case -1: /*the use didnt pu a file*/
+		printf("ERROR: there is no file. \n");
+
+        case 0: 
+            	break;
+
+        case 1: /*name error*/
+            	printf("ERROR: the file name is incorrect.");
+            	break;
+
+ 	case 2: /*empty file error*/
+            	printf("ERROR: the file is not exist.");
+            	break;
+
+
+       /*default:*/
+
+    	}
+
+}/*end printError func*/
+
+
+
+
+/*adds the non "." option!!!!*/
+int nameCheckFile(char* fileName)
+{
+	int pointFlag = CORRECT;
+	const char ch[1] = "."; 
+	char *checkSuffix; 
+
+	checkSuffix = strstr(fileName, ch);	
+	pointFlag = (strcmp(checkSuffix, ".as") == CORRECT)?CORRECT:NAME_ERROR; /*the suffix is incorrect . [not ".as"]*/
+
+	return pointFlag;	
+
+}
