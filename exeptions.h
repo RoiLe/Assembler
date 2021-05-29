1012e056a358862b99c06ccbb8869efdd1266860
Roi Levi <74343893+RoiLe@users.noreply.github.com>
2021-05-29 22:58:22 +0300

    Create exeptions.h

---
+++ exeptions.h
@@ -0,0 +1,10 @@
+#ifndef EXEPTIONS_H
+#define EXEPTIONS_H
+
+/*prints the error if there is some*/
+void printErrors(int);
+
+/*check the name of the file and if the file is empty.*/
+int nameCheckFile(char*);
+
+#endif
