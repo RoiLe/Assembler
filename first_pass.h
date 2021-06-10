894401704bd97f4c73f0e40824d4f6db6a4516a4
Roi Levi <74343893+RoiLe@users.noreply.github.com>
2021-06-10 21:34:53 +0300

    Update first_pass.h

--- first_pass.h
+++ first_pass.h
@@ -1,5 +1,22 @@
 /*- קובץ Header המחזיק את כל ההגדרות הרלוונטיות לfirstpass.c*/
-readline
-classificationOfSentence(char*);
-instructionLine()
-guidanceLine()
+#ifndef FIRSTPASS_H
+#define FIRSTPASS_H
+
+
+
+/*the proccess of the first passing assembler*/
+
+/*take only the instructions line and organize them in table by the required order*/
+void data_image_line_create(DI_ptr *, char*, long);
+
+symLine *add_to_symbol_table( char *, long);
+
+void free_symbol_table_nodes(symLine *);
+
+void free_data_image_nodes(dataImg*);
+
+/*vvvvvvv---delete before the submission--vvvvvvvvvvvvv */
+void data_image_print(DI_ptr);
+void symbol_table_print(symLine*);/*delete before the submission. */
+
+#endif
