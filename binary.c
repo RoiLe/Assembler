9a86279ba586c364d45736ab28af98f2569fc3a9
Roi Levi <74343893+RoiLe@users.noreply.github.com>
2021-06-10 21:37:41 +0300

    Create binary.c

---
+++ binary.c
@@ -0,0 +1,25 @@
+/*1. buildLRogicsInstructions( ls, rt, rd, funct) --> R
+2. buildRCopyInstructions(rs, rd, funct) --> R
+3. buildILogicsInstructions(funct) --> I
+4. buildIBranchesInstructions(funct) --> I
+5. buildIStoreLoadInstructions(funct) --> I
+6. j!*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include "globals.h"
+#include "utils.h"
+
+char *binaryLine(int typeOfSentence/*, ...*/)
+{
+	char *machineCode = "0010010111 tryyy";
+	/*va_list argPtr;
+	va_start(argPtr, typeOfSentence);
+
+	
+
+	va_args(pointer, sizeof)
+	va_end(argPtr);*/
+	return machineCode;
+}
