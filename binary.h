87640e5747e60272cee6854c9378366603a905f9
Roi Levi <74343893+RoiLe@users.noreply.github.com>
2021-06-10 21:38:04 +0300

    Create binary.h

---
+++ binary.h
@@ -0,0 +1,28 @@
+#ifndef BINARY_H
+#define BINARY_H
+
+#define MACHINE_CODE_SIZE 40
+
+/*
+
+	can get instruction and guidences line 
+*/
+char *binaryLine(int/*, ...*/);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
