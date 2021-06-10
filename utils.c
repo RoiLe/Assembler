d0ca279834a64ae7913ea14ad7605b1b4c8bc920
Roi Levi <74343893+RoiLe@users.noreply.github.com>
2021-06-10 21:35:43 +0300

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,22 +1,81 @@
-#include "global.h" 
-
-/*this func checks the name of the input file.*/
-int checkNameFile(*char);
-
-/*this func print the correct error*/
-printErrors(int);
-[
-case 0 : the file worked correct.
-case 1 : "file name error"
-]
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "globals.h" 
+#include "utils.h"
 
 
+/*int num_of_lines(FILE*);create!*/
 
 
+int classification_of_sentence(char* currLine)
+{
+	int emptyFlag = off, guidanceFlag = off;
+	int i = 0;
+ 	if(currLine[0] == ';'){return NOTE_LINE;}
 
-1. buildLRogicsInstructions( ls, rt, rd, funct) --> R
-2. buildRCopyInstructions(rs, rd, funct) --> R
-3. buildILogicsInstructions(funct) --> I
-4. buildIBranchesInstructions(funct) --> I
-5. buildIStoreLoadInstructions(funct) --> I
-6. j!
+	while(i < strlen(currLine))
+	{
+		if(currLine[i] != '\t' || currLine[i] != ' ')
+			{emptyFlag = on;}
+
+		if(currLine[i] == '.')
+			{guidanceFlag = on;}
+		i++;	
+	}
+	
+	if(emptyFlag == off)
+		{return EMPTY_LINE;}
+	if(guidanceFlag == on)
+		{return GUIDANCE_LINE;}
+	else
+		{return INSTRUCTION_LINE;}
+}/*END classificationOfSentence()*/
+
+
+char *find_the_label(char* currLine)
+{
+	int  i;
+	char *label = (char *)calloc(LABEL_MAX_LENGTH, sizeof(char));
+	char tmpString[LABEL_MAX_LENGTH];
+
+	for(i = 0; i < strlen(currLine); i++)
+	{
+		/*check the label*/
+		if(currLine[i] == ':') /*there is a label in the line.*/
+		{	
+
+			tmpString[i] = '\0' ;
+			strcpy(label, tmpString);
+			return label;
+		}
+
+		tmpString[i] = currLine[i]; /*if we dont shoure that there is a label.*/
+		
+	}/*END for loop*/
+
+	return NULL;
+}/*END find_the_label() */
+
+
+char *revese_string(char* str)
+{
+	char c, *start, *end;
+
+	if(!str || !*str)
+		return str;
+
+	start = str;
+	end = str+strlen(str)-1;
+
+	while(start < end)
+	{
+	 c=*start;
+	*start=*end;
+	*end=c;	
+	start++;
+	end--;
+	}
+	return str;
+	
+}/*END revese_string()*/
