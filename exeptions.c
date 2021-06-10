a1d6737515451e2a3b574ba244d69f5e23724162
Roi Levi <74343893+RoiLe@users.noreply.github.com>
2021-06-10 21:33:26 +0300

    Update exeptions.c

--- exeptions.c
+++ exeptions.c
@@ -1,5 +1,3 @@
-
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -9,25 +7,34 @@
 
 
 
-void printErrors(int errorType)
+
+void print_errors(int errorType, int numOfLine)
 {
 	switch(errorType)
     	{
-	case -1: /*the use didnt pu a file*/
-		printf("ERROR: there is no file. \n");
 
         case 0: 
             	break;
 
         case 1: /*name error*/
-            	printf("ERROR: the file name is incorrect.");
+            	printf("ERROR: the file name is incorrect.\n");
             	break;
 
  	case 2: /*empty file error*/
-            	printf("ERROR: the file is not exist.");
+            	printf("ERROR: the file is not exist.\n");
             	break;
 
+	case 3: /*sentence syntax error*/
+            	printf("ERROR: there is a syntax error in line %d.\n", numOfLine);
+            	break;
 
+	case 4: /*sentence syntax error*/
+            	printf("ERROR: the line %d is too long.\n", numOfLine);
+            	break;
+
+	case 5: /*sentence syntax error*/
+            	printf("ERROR: there is a problem to allocate memory.\n");
+            	break;
        /*default:*/
 
     	}
@@ -37,16 +44,45 @@ void printErrors(int errorType)
 
 
 
-/*adds the non "." option!!!!*/
-int nameCheckFile(char* fileName)
+int name_check_file(char* fileName)
 {
-	int pointFlag = CORRECT;
+	int suffixFlag = off, pointFlag = off, i;
 	const char ch[1] = "."; 
 	char *checkSuffix; 
+	
+	for( i= 0; i < strlen(fileName); i++)    /*check if there is dot ['.']*/
+	{	
+		if(fileName[i] == '.')
+			{pointFlag = on;}
+	}
+	if(pointFlag == off){return NAME_ERROR;}
 
 	checkSuffix = strstr(fileName, ch);	
-	pointFlag = (strcmp(checkSuffix, ".as") == CORRECT)?CORRECT:NAME_ERROR; /*the suffix is incorrect . [not ".as"]*/
+	suffixFlag = (strcmp(checkSuffix, ".as") == CORRECT)?CORRECT:NAME_ERROR; /*the suffix is incorrect . [not ".as"]*/
+	
+	return suffixFlag;	
 
-	return pointFlag;	
+}/*END nameCheckFile()*/
 
-}
+/*
+
+
+*/
+int check_line_syntax(char* currLine)
+{
+	int counter = 0, i;
+
+	for(i = 0; i < strlen(currLine); i++)
+	{
+		/*add check the label*/
+		/*add the guidence syntax check */
+		/*add the instruction syntax check */
+
+
+	}/*end for loop*/
+
+	if((counter == LINE_MAX_LENGTH) && (currLine[counter] != EOF))return SYNTAX_ERROR;
+	
+
+	return CORRECT;
+}/*END checkLineSyntax()*/
