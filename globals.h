ec392775bc316eb0b9a2b7771ed3e5aa89f61285
Roi Levi <74343893+RoiLe@users.noreply.github.com>
2021-07-04 21:06:49 +0300

    Update globals.h

--- globals.h
+++ globals.h
@@ -1,28 +1,69 @@
 /*- קובץ header הצהרות על פונקציות שמשתמשים בהם במהלך כל התוכנית (registers,opcode,instructions, IC&DC, symbol-table ודומיהם)*/
-#define IC 100
-#define DC 0	
+#define IC 100 
+#define DC 0	  
 #define NEXT_ADRESS 4
 #define LINE_MAX_LENGTH 80
+#define MAX_ARRTIBUTE_LENGTH 15
+#define KEY_WORD_MAX_LENGTH 10
 #define LABEL_MAX_LENGTH 31
+#define MACHINE_CODE_LENGTH 40 
 
-enum file_succeeded {
+
+
+enum file_succeeded { 
 	SUCCESSED = 0, FAILED = 1
 };
 
 enum flags {
 	off = 0, on = 1
-};
+};  
 
 typedef enum booleans {
 	FALSE = 0, TRUE = 1
 } bool;
+ 
 
-typedef enum arrtibutes {
-	CODE = 0,
-	DATA , 
-	ENTRY ,
-	EXTERNAL
-}arr;
+typedef enum type_of_instruction{
+
+	R_LINE_LOGIC = 1,
+	R_LINE_COPY,
+
+	 
+	I_LINE_LOGIC,
+	I_LINE_BRANCH,
+	I_LINE_SORE_LOAD, 
+
+	
+	J_LINE_SPECIAL
+
+}type_ins;
+
+typedef enum type_of_guidance{
+
+	DB = 1,
+	DH,
+	DW,
+	ASCIZ,
+	ENTRY,
+	EXTERN
+
+}type_guide;
+
+typedef enum funct{
+
+	/*R arithmetic instructions*/
+	FU_ADD = 1,
+	FU_SUB,
+	FU_AND,
+	FU_OR,
+	FU_NOR,
+
+	/*R copy instructions*/
+	FU_MOVE = 1,
+	FU_MVHI,
+	FU_MVLO
+
+}funct;
 
 typedef enum opcodes{
 
@@ -54,53 +95,23 @@ typedef enum opcodes{
 	OP_SH,
 
 	/*J opcodes*/
-	OP_JMP = 30,
-	OP_LA,
-	OP_CALL ,
-	OP_STOP = 63
-}opcode;
+	OP_JMP = 30, 
+	OP_LA, 
+	OP_CALL , 
+	OP_STOP = 63 
+}opcode;  
+ 
 
-/*typedef enum registers {
-	$0 = 0,
-	$1,
-	$2,
-	$3,
-	$4,
-	$5,
-	$6,
-	$7,
-	$8,
-	$9,
-	$10,
-	$11,
-	$12,
-	$13,
-	$14,
-	$15,
-	$16,
-	$17,
-	$18,
-	$19,
-	$20,
-	$21,
-	$22,
-	$23,
-	$24,
-	$25,
-	$26,
-	$27,
-	$28,
-	$29,
-	$30,
-	$31
-
-} reg;*/
-
+enum size_of_byte{
+	BYTE = 8,
+	HALF_WORD = 16, 
+	WORD = 32 	
+};
 
 typedef struct symbol_line{
 	char symbol[LABEL_MAX_LENGTH];
 	long value;
-	char *attribute;
+	char attribute[MAX_ARRTIBUTE_LENGTH]; 
 	struct symbol_line *next;
 }symLine;
 
@@ -108,44 +119,61 @@ typedef struct dataImage *DI_ptr;
 typedef struct dataImage{
 	long adress;
 	char sourceCode[LINE_MAX_LENGTH];
-	char machineCode[32];
+	char machineCode[MACHINE_CODE_LENGTH];
 	DI_ptr next;
 }dataImg;
 
 
-union instructionR{
+typedef union instructionR{
 	struct instruction_R_line{
-		unsigned int NONE: 5;
+		unsigned int NONE: 6;
 		unsigned int funct: 5;
 		unsigned int rd: 5;
 		unsigned int rt: 5;	
 		unsigned int rs: 5;
 		unsigned int opcode: 6;
 	}instruct;
-	int size: 31;
+	int size: 32;
 }R;
 
-union instructionI{
+typedef union instructionI{
 	struct instruction_I_line{
-		unsigned int immed: 15;
+		unsigned int immed: 16;
 		unsigned int rt: 5;	
 		unsigned int rs: 5;
 		unsigned int opcode: 6;
 	}instruct;
-	int size: 31;
+	int size: 32;
 }I;
 
-union instructionJ{
+typedef union instructionJ{
 	struct instruction_J_line{
-		unsigned int address: 24;	
+		unsigned int address: 25;	
 		unsigned int reg: 1;
 		unsigned int opcode: 6;
 	}instruct;
-	int size: 31;
+	int size: 32;
 }J;
 
 
+typedef union guidanceDB{
+	struct guidance_byte{
+		unsigned int byte: 8;	
+	}guidance;
+	int size: 8;
+}DB_CODE;
 
+typedef union guidanceDH{
+	struct guidance_half{
+		unsigned int half_word: 16;	
+	}guidance;
+	int size: 16;
+}DH_CODE;
 
-
+typedef union guidanceDW{
+	struct guidance_word{
+		unsigned int word: 32;	
+	}guidance;
+	int size: 32;
+}DW_CODE;
 
