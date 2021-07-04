b849dc113f8bc82a321f4053f80519d29d65f454
Roi Levi <74343893+RoiLe@users.noreply.github.com>
2021-07-04 21:05:14 +0300

    Update binary.h

--- binary.h
+++ binary.h
@@ -1,26 +1,46 @@
 #ifndef BINARY_H
 #define BINARY_H
 
-#define MACHINE_CODE_SIZE 40
+#define MAX_UNSIGNED_INT 32767
+#define MIX_UNSIGNED_INT -32768
 
 /*
+	Creates a binary line from instructions lines in 
+	every case of instruction line : R, I, J and specials.
 
-	can get instruction and guidences line 
+	**The variables**
+	curr_R_line -> current line of R structure [opcode, rs, rt, rd, funct, NONE]
+	curr_I_line -> current line of I structure [opcode, rs, rt ,immed]
+	curr_J_line -> current line of J structure [opcode, reg, address]
+	curr_opcode -> the opcode code number
+	curr_funct -> The funct code number
+	type_of_key_word -> the type of the instruction line[R_LINE_LOGIC, R_LINE_COPY, I_LINE_LOGIC, I_LINE_BRANCH, I_LINE_SORE_LOAD, J_LINE_SPECIAL]
+	 
+	input output example: 	input -> add $3,$5,$9
+				output -> 00000000011001010100100001000000
+				
 */
-char *binaryLine(int/*, ...*/);
+void instruction_binary_line(char *machineCode, char *key_word , char *operands);
 
 
 
+/*
+	Creates a binary line from instructions lines in 
+	every case of instruction line : R, I, J and specials.
 
-
-
-
-
-
-
-
-
-
+	**The variables**
+	curr_db_asciz -> current line of db and asciz structure 
+	curr_dh -> current line of dh structure 
+	curr_dw -> current line of dw  structure 
+	
+	type_of_guidance -> the type of the guidances line[DB, DH, DW, ASCIZ, ENTRY, EXTERN]
+	 
+	input output example: 	input -> K: .dw 31,-12
+				output ->00000000000000000000000000011111
+					11111111111111111111111111110100
+	
+*/
+void guidance_binary_lines(char *machineCode,char *key_word, int data);
 
 
 
