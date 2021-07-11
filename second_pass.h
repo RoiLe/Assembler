307dbf013b811c4eed33ce47251f9f268ab685df
Roi Levi <74343893+RoiLe@users.noreply.github.com>
2021-07-11 22:35:50 +0300

    Update second_pass.h

--- second_pass.h
+++ second_pass.h
@@ -1,1 +1,30 @@
-/*- קובץ Header המחזיק את כל ההגדרות הרלוונטיות לsecondpass.c*/
+#ifndef SECONDPASS_H
+#define SECONDPASS_H
+
+
+/*
+
+
+*/
+void second_pass(DI_ptr instruction_head, symLine *symbol_table_head, char *curr_line);
+
+
+/*
+	adds the entry attrubutes to the symbol table, and check if there is a problem with this.
+
+*/
+int add_entry_to_symbol_table(symLine *symbol_table_head, char *operand);
+
+/*
+
+
+*/
+void add_missed_values_to_data_image(DI_ptr instruction_head,symLine *symbol_table_head,char *key_word, char *operands, char *curr_line);
+
+/*
+
+
+*/
+int check_reg_flag(DI_ptr head_data_image, char *curr_line);
+
+#endif
