b14dd4acd90a3e738efe49c0f86758a6e8b5808b
Roi Levi <74343893+RoiLe@users.noreply.github.com>
2021-07-21 15:00:39 +0300

    Update to_file.h

--- to_file.h
+++ to_file.h
@@ -1,1 +1,26 @@
+#ifndef TOFILES_H
+#define TOFILES_H
 
+
+/*
+	main program that called the other function to create the files.
+	[.ob | .ent | .ext]
+*/
+void outputs(symLine *symbol_table_head, DI_ptr data_image_head, long ic, long dc, char *the_name);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
