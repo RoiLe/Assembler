2207d7f7e12c243a27602753e8b0bebdb19b6c64
Roi Levi <74343893+RoiLe@users.noreply.github.com>
2021-07-21 14:58:45 +0300

    Update exeptions.h

--- exeptions.h
+++ exeptions.h
@@ -4,19 +4,19 @@
 
 typedef enum errors{
 	CORRECT = 0,
-	NAME_ERROR ,
+	NAME_ERROR ,  
 	DONT_EXIST_FILE_ERROR ,
 	SYNTAX_ERROR, 
-	TOO_LONG_LINE_COMMAND_ERROR,
+	TOO_LONG_LINE_COMMAND_ERROR, 
 	ALLOCATION_ERROR/*,
-	LENGTH_LABEL_ERROR*/
+	LENGTH_LABEL_ERROR*/ 
 }err;
 
-/*prints the error if there is some*/
+/*prints the error if there is some*/ 
 void print_errors(int, int);
 
 /*check the name of the file and if the file is empty.*/
-int name_check_file(char*);
+int name_check_file(char* file_name, char *the_name);
 
 /*check the syntax of the line*/
 int check_line_syntax(char*);
