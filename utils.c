cd9d9e4db7f0818d863e9b71b41524e61aebefc5
Roi Levi <74343893+RoiLe@users.noreply.github.com>
2021-07-23 14:14:28 +0300

    Update utils.c

--- utils.c
+++ utils.c
@@ -349,7 +349,17 @@ void get_last_field(char *operands, char* label)
 }/*END get_last_field()*/
 
 
+int compare_strings(char *str1, char *str2){
+	int i = 0;
+	if(str1 == NULL || str2 == NULL){return FALSE;}
+	while (str1[i] != '\0' && str2[i] != '\0')
+	{
+		if(str1[i] !=  str2[i]){return FALSE;}
+		i++;
+	}
 
+	return TRUE;
+}/*END compare_strings() */
 
 
 
