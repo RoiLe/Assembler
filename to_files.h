e212ff6b5f92b4bafda39fdbbb84eb055e362bf5
Roi Levi <74343893+RoiLe@users.noreply.github.com>
2021-08-03 23:10:58 +0300

    Update to_files.h

--- to_files.h
+++ to_files.h
@@ -3,19 +3,14 @@
 
 
 /*
-	main program that called the other function to create the files.
+	Main program that called the other function to create the files.
 	[.ob | .ent | .ext]
+
+	object file -> .ob -> store the machine code.
+	enrty file -> .ent -> store all the symbols that difined as entry.
+	external file -> .ext -> store all the symbols that difined as external.  
 */
 void outputs(symLine *symbol_table_head, DI_ptr data_image_head, long ic, long dc, char *the_name);
-
-
-
-
-
-
-
-
-
 
 
 
